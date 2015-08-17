6ce040b94cd2fe4a42e88da790284d1fb15813dc
Suraj Kumar Sau <i@surajsau.in>
2015-08-17 16:00:24 +0530

    first commit

---
+++ typecheat.js
@@ -0,0 +1,34 @@
+wordNumber = 0;
+function PrintNextWord() {
+    word = answerArr[wordNumber];
+    inputs[0].value = word;
+    wordNumber++;
+}
+
+// setup - run once
+
+document.onkeypress = PrintNextWord;
+
+firstWord = null;
+for (i=0; i<99; ++i) {
+    firstWord = document.getElementById("nhwMiddlegwt-uid-" + i);
+    if (firstWord != null) {
+        break;
+    }
+}
+
+answer = null;
+for (i=0; i<99; ++i) {
+    answer = document.getElementById("nhwRightgwt-uid-" + i);
+    if (answer != null) {
+        break;
+    }
+}
+
+answer = firstWord.innerHTML + answer.innerHTML;
+// alert(answer);
+answerArr = answer.split(" ");
+
+inputs = document.getElementsByTagName("input");
+
+PrintNextWord();
